efc7782cabb8ed781f08c5988610903c426f5aee
Felipe Fritzen <felipefritzen94@hotmail.com>
2017-12-12 19:47:55 -0200

    First and follow

--- js/parserLR-functions.js
+++ js/parserLR-functions.js
@@ -37,10 +37,13 @@ $(document).ready(function () {
                             {NT: "E", follow: ")"},
                             {NT: "C", follow: "$"}];*/
 
+            var follows = buscaFirstFollow(gramatica);
+            
+            /*
             var follows = [{NT: "E", follow: "+|)|$"},
                             {NT: "T", follow: "+|*|)|$"},
                             {NT: "F", follow: "+|*|)|$"}];
-
+            */
             /***************************************************************************/
 
             construcaoTabelaSLR(gramatica, follows);
@@ -68,187 +71,136 @@ $(document).ready(function () {
 
     function buscaFirstFollow(gramatica) {
         //Inicializa array do first e follow no objeto gramatica
-        gramatica.first = [];
-        gramatica.follow = [];
-
+        var first = [];  
+        var follow = [];
+        for(var j = 0; j < gramatica.naoTerminais.length; j++){
+            first[j] = {};
+            first[j].NT = gramatica.naoTerminais[j];
+            first[j].first = '';
+            follow[j] = {};
+            follow[j].NT = gramatica.naoTerminais[j];
+            if(gramatica.naoTerminais[j] === gramatica.simboloInicio){
+                follow[j].follow = '$';
+            } else {
+                follow[j].follow = '';
+            }
+            
+        }
         //Percorre produções, gerando o first do não terminal em cada iteração
         for (var i = (gramatica.producoes.length - 1); i >= 0; i--) {
-            gramatica = buscaFirst(gramatica, gramatica.producoes[i].naoTerminais);
-            gramatica.first[i].first = unique(gramatica.first[i].first);
+            first = buscaFirst(gramatica, first, gramatica.producoes[i].naoTerminais, gramatica.producoes[i].complemento);
         }
-
-        //Inicializa o follow do símbolo de início de produção da gramática
-        var indexFirstFollow = gramatica.producoes.findIndex(x => x.naoTerminais == gramatica.simboloInicio);
-        gramatica.follow[indexFirstFollow] = {};
-        gramatica.follow[indexFirstFollow].naoTerminal = gramatica.simboloInicio;
-        gramatica.follow[indexFirstFollow].follow = '$';
-
-        //Percorre produções, gerando o follow do não terminal em cada iteração
-        for (var t = 0; t < gramatica.producoes.length; t++) {
-            gramatica = buscaFollow(gramatica, gramatica.producoes[t].naoTerminais);
+        //Chama função que percorre as produções, gerando o follow dos terminais
+        follow = buscaFollow(gramatica, follow);
+        /*
+        for(var g = 0; g < first.length; g++){
+            alert('first[' + g + '].NT: '     + first[g].NT
+              +'\n first[' + g + '].first: '  + first[g].first
+              +'\n follow['+ g + '].NT: '     + follow[g].NT
+              +'\n follow['+ g + '].follow: ' + follow[g].follow);
         }
-        for (var t = 0; t < gramatica.producoes.length; t++) {
-            gramatica.follow[t].follow = unique(gramatica.follow[t].follow);
-            if(gramatica.follow[t].follow.indexOf('&')>-1){
-                var arrayTemp = gramatica.follow[t].follow.replace(/\s/g, '').split(',');
-                for(key in arrayTemp){
-                    if(arrayTemp[key].indexOf('&')>-1){
-                        arrayTemp.splice(key, 1);
-                    }
-                }
-                gramatica.follow[t].follow = arrayTemp.join(", ");
-            }
-        }
-        
-        return gramatica;
-    }
-
-    var controlNullFirst = [];
-
-    // Busca os first da gramática
-    function buscaFirst(gramatica, simbolo) {
-        //Buscar first de todos os não-terminais e armazenar no objeto aqui
-        var firstIndex = gramatica.producoes.findIndex(x => x.naoTerminais == simbolo);
-        var producoesAux = gramatica.producoes[firstIndex].complemento.replace(/\s/g, '').split("|");
-
-        //Inicializa o atributo first do símbolo analisado
-        gramatica.first[firstIndex] = {};
-        gramatica.first[firstIndex].first = '';
-        gramatica.first[firstIndex].naoTerminal = simbolo;
-
-        //Gera o first do símbolo analisado, percorrendo as produções deste símbolo
-        for (var j = 0; j < producoesAux.length; j++) {
-            //Caso o primeiro elemento da produção seja um terminal, é adicionado ao first
-            if (producoesAux[j].charAt(0) === producoesAux[j].charAt(0).toLowerCase()) {
-                gramatica.first[firstIndex].first += gramatica.first[firstIndex].first == '' ? producoesAux[j].charAt(0) : (', ' + producoesAux[j].charAt(0));
-            } else if (producoesAux[j].charAt(0) === producoesAux[j].charAt(0).toUpperCase()) {
-                //Caso o primeiro elemento seja um não terminal, inicia a verificação do first deste não terminal
-                var naoTerminalIndex = gramatica.producoes.findIndex(x => x.naoTerminais == producoesAux[j].charAt(0));
-                if (gramatica.producoes[naoTerminalIndex].complemento.indexOf('&') > -1) {
-                    if (producoesAux[j].length !== 1) {
-                        var naoTerminalProximoIndex = gramatica.producoes.findIndex(x => x.naoTerminais == producoesAux[j].charAt(1));
-                        if (gramatica.first[naoTerminalIndex] !== undefined) {
-                            gramatica.first[firstIndex].first += gramatica.first[firstIndex].first == '' ? gramatica.first[naoTerminalIndex].first : (', ' + gramatica.first[naoTerminalIndex].first);
-                        } else if (gramatica.first[naoTerminalIndex] === undefined) {
-                            var auxArray = [firstIndex, naoTerminalIndex];
-                            controlNullFirst.push(auxArray);
-                        }
-                        if (producoesAux[j].charAt(1) === producoesAux[j].charAt(1).toLowerCase()) {
-                            gramatica.first[firstIndex].first += gramatica.first[firstIndex].first == '' ? producoesAux[j].charAt(1) : (', ' + producoesAux[j].charAt(1));
-                        }
-                        else if (producoesAux[j].charAt(1) === producoesAux[j].charAt(1).toUpperCase()) {
-                            if (gramatica.first[naoTerminalIndex] !== undefined) {
-                                gramatica.first[firstIndex].first += gramatica.first[firstIndex].first == '' ? gramatica.first[naoTerminalIndex].first : (', ' + gramatica.first[naoTerminalIndex].first);
-                            } else if (gramatica.first[naoTerminalIndex] === undefined) {
-                                var auxArray = [firstIndex, naoTerminalIndex];
-                                controlNullFirst.push(auxArray);
-                            }
-                        }
-                    } else {
-                        if (gramatica.first[naoTerminalIndex] !== undefined) {
-                            gramatica.first[firstIndex].first += gramatica.first[firstIndex].first == '' ? gramatica.first[naoTerminalIndex].first : (', ' + gramatica.first[naoTerminalIndex].first);
-                        } else if (gramatica.first[naoTerminalIndex] === undefined) {
-                            var auxArray = [firstIndex, naoTerminalIndex];
-                            controlNullFirst.push(auxArray);
-                        }
-                    }
-                } else {
-                    //Caso o first do não terminal não tenha sido resolvido ainda, seta como pendência para resolver depois
-                    if (gramatica.first[naoTerminalIndex] !== undefined) {
-                        gramatica.first[firstIndex].first += gramatica.first[firstIndex].first == '' ? gramatica.first[naoTerminalIndex].first : (', ' + gramatica.first[naoTerminalIndex].first);
-                    } else if (gramatica.first[naoTerminalIndex] === undefined) {
-                        var auxArray = [firstIndex, naoTerminalIndex];
-                        controlNullFirst.push(auxArray);
-                    }
-                }
-            }
-        }
-        //Resolve as pendências dos first
-        if (controlNullFirst[0] !== undefined && simbolo === gramatica.producoes[0].naoTerminais) {
-            for (var u = 0; u < controlNullFirst.length; u++) {
-                var firstIndexAux = controlNullFirst[u][0];
-                var naoTerminalIndexAux = controlNullFirst[u][1];
-                gramatica.first[firstIndexAux].first += gramatica.first[firstIndexAux].first == '' ? gramatica.first[naoTerminalIndexAux].first : (', ' + gramatica.first[naoTerminalIndexAux].first);
-            }
-        }
-        return gramatica;
-    }
-
-    //Remove elementos repetidos da sentença
-    function unique(str) {
-        str = str.replace(/[ ]/g, "").split(",");
-        var result = [];
-        for (var i = 0; i < str.length; i++) {
-            if (result.indexOf(str[i]) == -1) result.push(str[i]);
-        }
-        result = result.join(", ");
-        return result;
+        */
+        return follow;
     }
 
     var controlNullFollow = [];
     // Busca os follows da gramática
-    function buscaFollow(gramatica, simbolo) {
-        //Buscar follow de todos os não-terminais e armazenar no objeto aqui
-        var firstIndex = gramatica.producoes.findIndex(x => x.naoTerminais == simbolo);
-        //Inicializa o atributo follow para o símbolo analisado
+    function buscaFollow(gramatica, follow) {
+        for(var t = 0; t < gramatica.naoTerminais.length; t++){
+            for(var q = 0; q < gramatica.producoes.length; q++){
+                if(gramatica.producoes[q].complemento.indexOf(follow[t].NT) > -1){
+                    //Transforma a sentença analisada em um array
+                    var complementoArray = gramatica.producoes[q].complemento.split(" ");
+                    //Posição que o elemento se encontra dentro da sentença analisada
+                    var indexComplemento = complementoArray.indexOf(follow[t].NT)                    
+                    //Se o próximo elemento não existe (T -> F) := follow(F)=follow(T)
+                    if(complementoArray[indexComplemento+1] === undefined){
+                        //Pega o indice do não-terminal que produz a sentença, para buscar o follow dele
+                        var indiceAux = gramatica.naoTerminais.indexOf(gramatica.producoes[q].naoTerminais);
+                        //Follow do elemento é o follow do não-terminal que produz a sentença
+                        if (follow[indiceAux].follow !== undefined) {
+                            follow[t].follow += follow[t].follow == '' ? follow[indiceAux].follow : ('|' + follow[indiceAux].follow)
+                        } else if (follow[indiceAux].follow === undefined) {
+                            controlNullFollow.push([t, indiceAux]);
+                        }
+                        
+                    } else 
+                    //Se o próximo elemento é terminal (T -> E + T) := follow(E)=+
+                    if(gramatica.terminais.indexOf(complementoArray[indexComplemento+1]) > -1){
+                        //Próximo elemento é follow do elemento
+                        follow[t].follow += follow[t].follow == '' ? complementoArray[indexComplemento+1] : ('|' + complementoArray[indexComplemento+1])
+                    } else 
+                    //Se o próximo elemento é não-terminal (T -> E T) := follow(E)=first(T)
+                    if(gramatica.naoTerminais.indexOf(complementoArray[indexComplemento+1]) > -1){
+                        //Pega o indice do first, para buscar o first que será o follow do elemento
+                        var indiceAux = gramatica.naoTerminais.indexOf(complementoArray[indexComplemento+1]);
+                        //Coloca o first do próximo elemento, como follow do elemento
+                        follow[t].follow += follow[t].follow == '' ? first[indiceAux].first : ('|' + first[indiceAux].first)
+                    }    
+                }
+                    
+            }
+            follow[t].follow = unique(follow[t].follow)
+        }
+        return follow;
+    }
 
-        if (gramatica.follow[firstIndex] === undefined) {
-            gramatica.follow[firstIndex] = {};
-            gramatica.follow[firstIndex].naoTerminal = simbolo;
-            gramatica.follow[firstIndex].follow = '';
-        }
-        //Constroi uma matriz das produções da grámatica, para buscar as ocorrências dos não terminais
-        var prodAux = [];
-        for (var i = 0; i < gramatica.producoes.length; i++) {
-            prodAux[i] = gramatica.producoes[i].complemento.replace(/\s/g, '').split("|");
-        }
-        //Iteração das produçõs, para definir o follow do não terminal analisado
-        for (var j = 0; j < prodAux.length; j++) {
-            for (var k = 0; k < prodAux[j].length; k++) {
-                if (prodAux[j][k].indexOf(simbolo) > -1) {
-                    //Caso o símbolo analisado seja o último elemento da produção, seleciona o follow do não terminal que produz a sentença analisada
-                    if (prodAux[j][k].indexOf(simbolo) === (prodAux[j][k].length - 1)) {
-                        if (simbolo !== gramatica.producoes[j].naoTerminais) {
-                            var naoTerminalIndex = gramatica.producoes.findIndex(x => x.naoTerminais == gramatica.producoes[j].naoTerminais);
-                            if (gramatica.follow[naoTerminalIndex] !== undefined) {
-                                gramatica.follow[firstIndex].follow += gramatica.follow[firstIndex].follow == '' ? gramatica.follow[naoTerminalIndex].follow : (', ' + gramatica.follow[naoTerminalIndex].follow);
-                            } else if (gramatica.follow[naoTerminalIndex] === undefined) {
-                                var auxArray = [firstIndex, naoTerminalIndex];
-                                controlNullFollow.push(auxArray);
-                            }
-                        }
-                    } else if (prodAux[j][k].indexOf(simbolo) < (prodAux[j][k].length - 1)) {
-                        //Se o próximo elemento do símbolo analisado for um não terminal, seleciona o first daquele elemento
-                        if (prodAux[j][k].charAt((prodAux[j][k].indexOf(simbolo) + 1)) === prodAux[j][k].charAt((prodAux[j][k].indexOf(simbolo) + 1)).toUpperCase()) {
-                            var naoTerminalIndex = gramatica.producoes.findIndex(x => x.naoTerminais == prodAux[j][k].charAt((prodAux[j][k].indexOf(simbolo) + 1)));
-                            gramatica.follow[firstIndex].follow += gramatica.follow[firstIndex].follow == '' ? gramatica.first[naoTerminalIndex].first : (', ' + gramatica.first[naoTerminalIndex].first);
-                            //Se este não terminal produzir sentença vazia, também seleciona o follow dele
-                            if(gramatica.producoes[naoTerminalIndex].complemento.indexOf('&') > -1){
-                                if (gramatica.follow[naoTerminalIndex] !== undefined) {
-                                    gramatica.follow[firstIndex].follow += gramatica.follow[firstIndex].follow == '' ? gramatica.follow[naoTerminalIndex].follow : (', ' + gramatica.follow[naoTerminalIndex].follow);
-                                } else if (gramatica.follow[naoTerminalIndex] === undefined) {
-                                    var auxArray = [firstIndex, naoTerminalIndex];
-                                    controlNullFollow.push(auxArray);
-                                }    
-                            }
-                        } else if (prodAux[j][k].charAt((prodAux[j][k].indexOf(simbolo) + 1)) === prodAux[j][k].charAt((prodAux[j][k].indexOf(simbolo) + 1)).toLowerCase()) {
-                            //Se o próximo elemento do simbolo analisado for um terminal, adiciona o elemento como follow
-                            gramatica.follow[firstIndex].follow += gramatica.follow[firstIndex].follow == '' ? prodAux[j][k].charAt((prodAux[j][k].indexOf(simbolo) + 1)) : (', ' + prodAux[j][k].charAt((prodAux[j][k].indexOf(simbolo) + 1)));
-                        }
-                    }
+    function controlNullFollowFunction(first) {
+        if (controlNullFollow[0] !== undefined) {
+                for (var u = 0; u < controlNullFollow.length; u++) {
+                    var followIndexAux = controlNullFollow[u][0];
+                    var naoTerminalIndexAux = controlNullFollow[u][1];
+                    follow[followIndexAux].follow += follow[followIndexAux].follow == '' ? follow[naoTerminalIndexAux].follow : ('|' + follow[naoTerminalIndexAux].follow);
                 }
             }
-        }
-        //Resolve as pendências dos follows
-        if (controlNullFollow[0] !== undefined && simbolo === gramatica.producoes[gramatica.producoes.length-1].naoTerminais) {
-            for (var u = 0; u < controlNullFollow.length; u++) {
-                var firstIndexAux = controlNullFollow[u][0];
-                var naoTerminalIndexAux = controlNullFollow[u][1];
-                gramatica.follow[firstIndexAux].follow += gramatica.follow[firstIndexAux].follow == '' ? gramatica.follow[naoTerminalIndexAux].follow : (', ' + gramatica.follow[naoTerminalIndexAux].follow);
+    }
+
+    var controlNullFirst = [];
+    // Busca os first da gramática
+    function buscaFirst(gramatica, first, naoTerminais, terminais) {
+        
+        var terminaisArray = terminais.split(" ");
+        index = gramatica.naoTerminais.indexOf(naoTerminais);
+        
+        if(gramatica.terminais.indexOf(terminaisArray[0]) > -1){
+            
+            first[index].first += first[index].first == '' ? terminaisArray[0] : ('|' + terminaisArray[0])     
+            
+        } else if(gramatica.naoTerminais.indexOf(terminaisArray[0]) > -1 && terminaisArray[0] !== naoTerminais){
+            indexAux = gramatica.naoTerminais.indexOf(terminaisArray[0]);
+            if (first[indexAux].first !== undefined) {
+                first[index].first += first[index].first == '' ? first[indexAux].first : ('|' + first[indexAux].first);
+            } else if (first[indexAux].first === undefined) {
+                controlNullFirst.push([index, indexAux]);
             }
         }
-        return gramatica;
+        
+        first[index].first = unique(first[index].first)
+        return first;
     }
+
+    function controlNullFirstFunction(first) {
+        if (controlNullFirst[0] !== undefined) {
+                for (var u = 0; u < controlNullFirst.length; u++) {
+                    var firstIndexAux = controlNullFirst[u][0];
+                    var naoTerminalIndexAux = controlNullFirst[u][1];
+                    first[firstIndexAux].first += first[firstIndexAux].first == '' ? first[naoTerminalIndexAux].first : ('|' + first[naoTerminalIndexAux].first);
+                }
+            }
+    }
+
+    //Remove elementos repetidos da sentença
+    function unique(str) {
+        str = str.replace(/[ ]/g, "").split("|");
+        var result = [];
+        for (var i = 0; i < str.length; i++) {
+            if (result.indexOf(str[i]) == -1) result.push(str[i]);
+        }
+        result = result.join("|");
+        return result;
+    }
+
+    
 
     /*********************** FIM DAS FUNÇÕES DE FIRST E FOLLOW ***********************/
 
